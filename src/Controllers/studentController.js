1eaddc1c35b31fceabdffc5707f77b1d68f2eb0c
Manish Shrestha <shresthamanish606@gmail.com>
2023-11-29 07:20:03 +0545

    save changes

--- src/Controllers/studentController.js
+++ src/Controllers/studentController.js
@@ -27,7 +27,19 @@ export let readStudent = async(req,res)=>{
         // let result = await Student.find({age:{$lt:21}});
         // let result = await Student.find({name:{$ne:"Manish"}});
         // let result = await Student.find({age:{$gte:20}});
-        let result = await Student.find({age:{$gte:15,$lte:25}});
+        // let result = await Student.find({age:{$gte:15,$lte:25}});
+        // finding those whose name is given specific
+        // let result = await Student.find({name:{$in:["Manish","Sachin"]}})
+        // let result = await Student.find({$or:[{name:"Manisha"}, {name:"Sachin"}]});
+        // let result = await Student.find({$or:[{name:"Samira", age:28}, {name:"Manish"}]});
+        // let result = await Student.find({$and:[{name:"Manish"},{age:24}]});
+        // let result = await Student.find({$and:[{age:{$gte:15}},{age:{$lte:20}}]});
+        // find those user which doesn't conatain name: Manish, Manisha
+        // let result = await Student.find({$and:[{name:{$ne:"Manish"}}, {name:{$ne:"Manisha"}}]});
+        let query = req.query
+        // let result = await Student.find({$and:[{name:query.name},{age:query.age}]})
+        let result = await Student.find(query)
+        console.log(query)
         res.json(
             {
                 success : true,
